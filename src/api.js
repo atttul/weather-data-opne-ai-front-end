8ed1835bff161a43135b45c70f76b9576e6c8a25
Atul Akash <akashatul123@gmail.com>
2025-08-08 23:28:34 +0530

    console.log added for

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const API_BASE_URL = process.env.API_BASE_URL;
 
 export const getWeatherData = async (prompt) => {
     try {
+        console.log("API_BASE_URL", API_BASE_URL)
         const response = await axios.post(`${API_BASE_URL}/get/weather/data`, { prompt });
         return response.data;
     } catch (error) {
